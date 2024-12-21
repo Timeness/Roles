cc232813418af03a6d2f751b5c26bab066f7a7fc
Timeness <careless@flash.co>
2024-12-21 22:07:59 +0530

    Delete src/ub.py

--- src/ub.py
+++
@@ -1,6 +0,0 @@
-from apps import *
-
-userbot_client = Client("userbot", api_id=api_id, api_hash=api_hash, session_string=STRING_SESSION)
-
-userbot_client.run()
-print("🐾 Reporter userbot is running...")
